b9afb01f215ac6130bfeec25a28d25eaf245c7fe
Abhii <abhinavhada007@gmail.com>
2026-07-26 22:05:49 +0545

    prefer ninja for building

---
+++ build-test-2/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_jhatkaa_FILE D:/jhatkaa/build-test-2/src/jhatkaa.exe)
+set(__QT_DEPLOY_TARGET_jhatkaa_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_jhatkaa_RUNTIME_DLLS C:/msys64/ucrt64/bin/Qt6Widgets.dll;C:/msys64/ucrt64/bin/Qt6Gui.dll;C:/msys64/ucrt64/bin/Qt6Core.dll)
