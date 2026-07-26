9c056cc5711f860da816f7c27a06d3b0ebe43977
Abhii <157186373+abvyyii@users.noreply.github.com>
2026-07-26 22:07:26 +0545

    Delete build-test directory

--- build-test/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_jhatkaa_FILE D:/jhatkaa/build-test/src/jhatkaa.exe)
-set(__QT_DEPLOY_TARGET_jhatkaa_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_jhatkaa_RUNTIME_DLLS C:/msys64/ucrt64/bin/Qt6Widgets.dll;C:/msys64/ucrt64/bin/Qt6Gui.dll;C:/msys64/ucrt64/bin/Qt6Core.dll)
